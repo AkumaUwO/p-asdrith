692022d310e6b0807bbcd15a9974e50e4eb0a3ba
Ale035 <akumauwo2021@gmail.com>
2023-05-13 12:38:46 -0400

    portafolio-Asdrith-4

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+//funcion que aplica estilo a la opcion seleccionada en el menu y quita el estilo a la previamente seleccionada
 function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
+    var opciones = document.querySelectorAll("#links a");
     opciones[0].className = "";
     opciones[1].className = "";
     opciones[2].className = "";
     opciones[3].className = "";
     opciones[4].className = "";
+
     link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
-    var x = document.getElementById("nav");
+    //hacemos desaparecer el menu una vez que se ha selecionado la opcion en el modo responsive
+
+    var x = document.getElementById("nav"); 
     x.className = "";
 }
 
-//función que muestra el menu responsive
-function responsiveMenu() {
-    var x = document.getElementById("nav");
+//funcion que muestra el menu responsive
+
+function responsiveMenu(){
+    var x = document.getElementById("nav"); 
     if (x.className === "") {
         x.className = "responsive";
     } else {
